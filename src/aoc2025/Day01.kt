ec9c7cda4ef85d9c6006db1ccb8533b90387599f
Pim van Gurp <pim@Pims-Mac-Studio.local>
2025-12-01 21:33:40 +0100

    Refactor

--- src/aoc2025/Day01.kt
+++ src/aoc2025/Day01.kt
@@ -28,14 +28,14 @@ object Day1 : Day<Rotations>(1, 2025) {
     override suspend fun part2(input: Rotations) = input.fold(50 to 0) { (position, zeroCount), rotation ->
         val (direction, amount) = rotation
         val fullLoops = amount / 100
-        val countPastZero = when {
+        val countZeros = when {
             position == 0 -> false // Avoid duplicate counts; zero is counted when ending on this number
             direction == 'L' -> amount.mod(100) >= position
             direction == 'R' -> amount.mod(100) >= 100 - position
             else -> false
         }.toInt()
 
-        rotateDial(position, rotation) to zeroCount + fullLoops + countPastZero
+        rotateDial(position, rotation) to zeroCount + fullLoops + countZeros
     }.second
 
     private fun rotateDial(position: Int, rotation: Pair<Char, Int>): Int {
@@ -45,8 +45,6 @@ object Day1 : Day<Rotations>(1, 2025) {
             'L' -> position - amount
             'R' -> position + amount
             else -> throw Error()
-        }
-            .mod(100)
-            .let { if (it < 0) 100 - it else it }
+        }.mod(100)
     }
 }
